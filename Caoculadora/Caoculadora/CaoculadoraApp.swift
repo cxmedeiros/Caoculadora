5992043f371215a4cdbd7c3c99a0d8a12a5fa87f
Camila Xavier <camilaxmedeiros@gmail.com>
2024-05-07 16:11:10 -0300

    Add project files

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Camila Xavier de Medeiros on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
