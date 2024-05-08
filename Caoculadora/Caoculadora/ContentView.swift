9b2504b7a27a6604472afcc6e92f65a3cedaa3c0
Camila Xavier <camilaxmedeiros@gmail.com>
2024-05-08 18:01:25 -0300

    Add a image and a button with result

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State var years: Int? = nil // para aparecer o texto logo
     @State var months: Int? = nil
+    @State var result: Int?
     
     var body: some View {
         
@@ -27,9 +28,37 @@ struct ContentView: View {
             TextField(
                 "E quantos meses além disso ele tem.",
                 value: $months,
-                format: . number)
-        
+                format: . number
+            )
+            Text("Porte")
+            // aqui tem o segmented control
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+                    .font(.system(size: 30))
+            } else {
+                Image(.dog)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 20)
+            }
+            Button(action: {
+                result = 23
+            }, label: {
+                ZStack {
+                    Color(.blue)
+                    Text("Cãocular")
+                        .foregroundStyle(.white)
+                }
+            })
+            .cornerRadius(10)
+            .frame(height: 50)
+            
+            
         }
+        
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .bold()
